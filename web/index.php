9aa69ac0608a89d499521d47a654547f17dfbda1
Heejong Jo <heejong0326@gmail.com>
2025-10-28 20:23:20 +0900

    feat: set up database connection and server initialization

--- web/index.php
+++ web/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+$host = getenv('MYSQL_HOST');
+$port = getenv('MYSQL_PORT');
+$db   = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
+$mysqli = new mysqli($host, $user, $pass, $db, $port);
+
+if ($mysqli->connect_error) {
+    die("Database connection failed: " . $mysqli->connect_error);
+}
+
+echo "Database connected successfully!";
